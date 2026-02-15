1d9437bad46f2664086949b9b26c279563deb2a5
k3vwdd <kevwdiaz@outlook.com>
2026-02-15 15:21:10 -0500

    small update to totalVolumeCredits

--- chapter-1/refactorExample.js
+++ chapter-1/refactorExample.js
@@ -44,24 +44,25 @@ function usd(aNumber) {
   }).format(aNumber);
 }
 
-function statement(invoice) {
-  let totalAmount = 0;
+function totalVolumeCredits() {
   let volumeCredits = 0;
+  for (let perf of invoices[0].performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
+}
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
-    if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
-
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
 
